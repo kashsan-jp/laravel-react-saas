d717aa9311c2bbe6f876cf36ea8311bc16f25b5b
Kashsan <kashsan_jp@yahoo.co.jp>
2025-07-13 18:17:01 +0900

    2025.7.13.18:04

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,8 +1,9 @@
-<?php
+ <?php
 
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,9 +11,18 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    // @test
+     public function a_user_can_visit_the_home_page(): void
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
 
         $response->assertStatus(200);
     }
